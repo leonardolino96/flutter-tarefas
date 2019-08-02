fce5becc88890f2d52fa1ffab55d178165762200
Leonardo Lino <leonardolino96@gmail.com>
2019-08-02 16:10:49 -0300

    Implementação do layout iniciada

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _tarefaController = TextEditingController();
+  ScrollController _scrollController = new ScrollController();
+
   List _tarefas = [];
+
+  void _addTarefa() {
+    setState(() {
+      Map<String, dynamic> novaTarefa = Map();
+      novaTarefa["title"] = _tarefaController.text;
+      novaTarefa["ok"] = false;
+      _tarefaController.clear();
+      _tarefas.add(novaTarefa);
+      _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent + 56,
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.easeOut);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,10 +49,65 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Lista de tarefas"),
       ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              controller: _scrollController,
+              itemCount: _tarefas.length,
+              itemBuilder: (context, index) {
+                return CheckboxListTile(
+                  title: Text(_tarefas[index]["title"]),
+                  value: _tarefas[index]["ok"],
+                  secondary: CircleAvatar(
+                    backgroundColor:
+                        _tarefas[index]["ok"] ? Colors.green : Colors.indigo,
+                    child: Icon(
+                        _tarefas[index]["ok"] ? Icons.done : Icons.short_text,
+                        color: Colors.white),
+                  ),
+                  onChanged: (check) {
+                    setState(() {
+                      _tarefas[index]["ok"] = check;
+                    });
+                  },
+                );
+              },
+            ),
+          ),
+          Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
+            elevation: 8,
+            margin: EdgeInsets.only(top: 4),
+            child: Container(
+              padding: EdgeInsets.all(8),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      controller: _tarefaController,
+                      decoration: InputDecoration(
+                          isDense: true,
+                          border: OutlineInputBorder(),
+                          hintText: "Nova tarefa"),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  OutlineButton(
+                    child: Text("Adicionar"),
+                    onPressed: _addTarefa,
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 
-  // Retorna o arquivo .json onde as tarefas serão salvas ou 
+  // Retorna o arquivo .json onde as tarefas serão salvas ou
   // cria um novo arquivo se não houver um
   Future<File> _getFile() async {
     Directory directory = await getApplicationDocumentsDirectory();
